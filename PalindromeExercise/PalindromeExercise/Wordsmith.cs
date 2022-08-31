c93a1822138c4df3cdd493bd866e7829637a83e7
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-31 08:11:07 -0700

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            return str.SequenceEqual(str.Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using PalindromeExerciseTests;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("dog", false)]
+        [InlineData("madam", true)]
+
+        public void PalindromTest(string str, bool expected)
         {
+            var palindrome1 = new Wordsmith();
+
+            var actual = palindrome1.IsAPalindrome(str);
+
+            Assert.Equal(expected, actual);
 
         }
+
+
     }
 }
